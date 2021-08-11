f326aa9e9b1e74044b989aa6e5ee1e9cb9918147
luv-vuvdh <vandinhhoangvu@luvina.net>
2021-08-11 09:41:22 +0700

    fix limit of add row

--- funct.js
+++ funct.js
@@ -24,30 +24,34 @@ function showHideGroup(){
 function createNewRow(){
 	var tableAnswer = document.getElementById("tableAnswer");
 	var rowCount = tableAnswer.rows.length;
-	var row = tableAnswer.insertRow();
+	if (rowCount < 5) {
+		var row = tableAnswer.insertRow();
 
-	row.id = "id"+ (rowCount + 1);
+		row.id = "id"+ (rowCount + 1);
 
-	var column1 = row.insertCell(0);
-	var column2 = row.insertCell(1);
-	var column3 = row.insertCell(2);
+		var column1 = row.insertCell(0);
+		var column2 = row.insertCell(1);
+		var column3 = row.insertCell(2);
 
-	var element1 = document.createElement('input');
-	element1.type = "text";
-	element1.name = "question";
-	element1.class = "textBox";
-	element1.style.width = "500px";
-	element1.style.height = "25px";
-	column2.appendChild(element1);
+		var element1 = document.createElement('input');
+		element1.type = "text";
+		element1.name = "question";
+		element1.class = "textBox";
+		element1.style.width = "500px";
+		element1.style.height = "25px";
+		element1.style.fontweight = bold;
+		column2.appendChild(element1);
 
 
-	var element2 = document.createElement('input');
-	element2.type = "radio";
-	element2.name = "correct";
-	element2.class = "textBox";
-	column3.appendChild(element2);
+		var element2 = document.createElement('input');
+		element2.type = "radio";
+		element2.name = "correct";
+		element2.class = "textBox";
+		column3.appendChild(element2);
 
-	column1.innerHTML = "Answer " + (rowCount + 1);
+		column1.innerHTML = "Answer " + (rowCount + 1);
+	}
+	
 }
 
 /*
